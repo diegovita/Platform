dd95596e2486d0eabd686438465ebc28dd7e2cd2
diegoVita <diegovitann@gmail.com>
2024-07-01 18:50:15 -0300

    SettingDbContext, Adding Controller.

---
+++ BloggingPlatform/Controllers/BlogPostsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+
+    }
+}
---
+++ BloggingPlatform/Data/BloggingPlatformContext.cs
@@ -0,0 +1,11 @@
+﻿using BloggingPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggingPlatform.Data;
+
+public class BloggingPlatformContext :  DbContext
+{
+    public BloggingPlatformContext(DbContextOptions<BloggingPlatformContext> options) : base(options) { }
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
