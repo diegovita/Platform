d02e07af141688fa0e51525758e72cb1f4249801
diegoVita <diegovitann@gmail.com>
2024-07-08 19:48:18 -0300

    Adding decorators

--- BloggingPlatform/Messages/CreateBlogPost.cs
+++ BloggingPlatform/Messages/CreateBlogPost.cs
@@ -1,10 +1,13 @@
 ﻿using BloggingPlatform.Models;
 using MassTransit.Mediator;
+using System.ComponentModel.DataAnnotations;
 
 namespace BloggingPlatform.Messages;
 
 public record CreateBlogPost : Request<BlogPost>
 {
+    [Required]
     public string Title { get; set; }
+    [Required]
     public string Content { get; set; }
 }
